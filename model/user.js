9b6fb4bd582486d9cdd8ac063d8353302fa0ae19
amritsapkotadev <amritsapkota.dev@gmail.com>
2024-10-21 21:11:07 +0545

    user schema set

---
+++ model/user.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose');
+const userschema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+
+    },
+    phonenumber:{
+        type:Number,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+});
+const user=mongoose.model('user',userschema);
+module.exports=user;
