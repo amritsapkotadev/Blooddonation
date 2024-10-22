1704b88b3c02e9d4f8c971692a625fb71d04cce6
amritsapkotadev <amritsapkota.dev@gmail.com>
2024-10-22 17:39:35 +0545

    commit

--- index.js
+++ index.js
@@ -4,6 +4,5 @@
 
 import {AppRegistry} from 'react-native';
  import {name as appName} from './app.json';
- import Firstpage from './screens/mainappscreens/Firstpage';
- import Home from './screens/Home';
+import Home from './Home';
 AppRegistry.registerComponent(appName, () => Home);
